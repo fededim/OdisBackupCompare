77be05e122f6ddabe16f19c1a86fa3cd590cf034
Federico Di Marco <fededim@gmail.com>
2025-11-05 21:56:29 +0100

    improved publish script

--- publish.ps1
+++ publish.ps1
@@ -1,10 +1,11 @@
+$path = ".\OdisBackupCompare\bin\Release\net8.0\publish"
+Remove-Item $path -Recurse -Force
 dotnet publish /p:PublishProfile=Win-X64
 dotnet publish /p:PublishProfile=Win-Arm64
 dotnet publish /p:PublishProfile=MacOS-X64
 dotnet publish /p:PublishProfile=MacOS-Arm64
 dotnet publish /p:PublishProfile=Linux-X64
 dotnet publish /p:PublishProfile=Linux-Arm64
-$path = ".\OdisBackupCompare\bin\Release\net8.0\publish"
 Get-ChildItem $path -Include *.pdb -Recurse | Remove-Item
 copy .\OdisBackupCompare\SampleXML .\OdisBackupCompare\bin\Release\net8.0\publish -Recurse -Force
 Compress-Archive -Path $path\* -DestinationPath "$path\release.zip" -CompressionLevel Optimal -Force
