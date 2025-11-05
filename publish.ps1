f027f83706bd27e1ba95c985612f6e79d66217c1
Federico Di Marco <fededim@gmail.com>
2025-11-05 19:07:11 +0100

    minor fix

--- publish.ps1
+++ publish.ps1
@@ -7,4 +7,4 @@ dotnet publish /p:PublishProfile=Linux-Arm64
 $path = ".\OdisBackupCompare\bin\Release\net8.0\publish"
 Get-ChildItem $path -Include *.pdb -Recurse | Remove-Item
 copy .\OdisBackupCompare\SampleXML .\OdisBackupCompare\bin\Release\net8.0\publish -Recurse -Force
-Compress-Archive -Path $path -DestinationPath "$path\release.zip" -CompressionLevel Optimal -Force
+Compress-Archive -Path $path\* -DestinationPath "$path\release.zip" -CompressionLevel Optimal -Force
